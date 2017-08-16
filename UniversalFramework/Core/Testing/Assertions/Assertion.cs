509c31f8dd16647032ff78b9b27e261a8f0c9d4a
Vitaliy Dobriyan <dobriy88@yandex.ru>
2017-08-16 20:06:53 +0400

    Added soft assertions and corresponding tests

--- UniversalFramework/Core/Testing/Assertions/Assertion.cs
+++ UniversalFramework/Core/Testing/Assertions/Assertion.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Unicorn.Core.Testing.Assertions
 {
@@ -21,6 +22,55 @@ namespace Unicorn.Core.Testing.Assertions
             {
                 throw new AssertionError(message + "\n" + matcher.ToString());
             }
+        }
+    }
+
+
+
+    public class SoftAssertion
+    {
+        StringBuilder Errors;
+        bool IsSomethingFailed;
+        int ErrorCounter;
+
+        public SoftAssertion()
+        {
+            Errors = new StringBuilder();
+            IsSomethingFailed = false;
+            ErrorCounter = 1;
+        }
+
+        public SoftAssertion AssertThat(object _object, Matcher matcher)
+        {
+            AssertThat("", _object, matcher);
+            return this;
+        }
+
+
+        public SoftAssertion AssertThat(string message, object _object, Matcher matcher)
+        {
+            matcher.Description.Append("Expected ");
+            matcher.DescribeTo();
+            matcher.Description.AppendLine("").Append("But ");
+
+            
+
+            if (!matcher.Matches(_object))
+            {
+                if (!string.IsNullOrEmpty(message))
+                    message += "\n";
+
+                Errors.AppendLine($"Error {ErrorCounter++}").Append(message).Append(matcher.ToString()).Append("\n\n");
+                IsSomethingFailed = true;
+                
+            }
+            return this;
+        }
+
+        public void AssertAll()
+        {
+            if (IsSomethingFailed)
+                throw new AssertionError("\n" + Errors.ToString().Trim());
         }
     }
 
---
+++ UniversalFramework/Tests/UnitTests/AssertionsTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using ProjectSpecific.BO;
+using Unicorn.Core.Testing.Assertions;
+using static Unicorn.Core.Testing.Assertions.CoreMatchers;
+
+namespace Tests.UnitTests
+{
+    [TestFixture]
+    class AssertionsTest
+    {
+        [Test, Author("Vitaliy Dobriyan")]
+        public void IsNullMatcherPositiveTest()
+        {
+
+
+            Assert.Throws<AssertionError>(delegate {
+                SoftAssertion assert = new SoftAssertion();
+                assert.AssertThat("asd", IsEqualTo(("asd")))
+                    .AssertThat(2, IsEqualTo((2)))
+                    .AssertThat(new SampleObject(), IsEqualTo(new SampleObject("ds", 234)))
+                    .AssertThat(new SampleObject(), IsEqualTo(new SampleObject()))
+                    .AssertThat("bla-bla-bla message", new SampleObject(), IsEqualTo(23));
+
+                assert.AssertAll();
+
+            });
+        }
+    }
+}
