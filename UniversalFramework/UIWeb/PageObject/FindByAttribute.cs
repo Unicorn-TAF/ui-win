4de248f211ec4edf32e1af68935c73f0c19763b6
Vitaliy Dobriyan <dobriy88@yandex.ru>
2017-09-07 15:19:28 +0400

    Added basic page object implamentation

---
+++ UniversalFramework/UIWeb/PageObject/FindByAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Unicorn.UICore.Driver;
+
+namespace Unicorn.UIWeb.PageObject
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class FindByAttribute : Attribute
+    {
+        public FindByAttribute(By by, string locator)
+        {
+            By = by;
+            Locator = locator;
+        }
+
+        public By By { get; set; }
+
+        public string Locator { get; set; }
+    }
+}
