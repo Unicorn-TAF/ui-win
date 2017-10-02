19365f2d39ec9f619ccd41bd54a49a5d309042f3
Vitaliy Dobriyan <dobriy88@yandex.ru>
2017-10-03 00:14:54 +0400

    Suite now contains all bugs references even if there are duplicates

--- UniversalFramework/Core/Testing/Tests/SuiteOutcome.cs
+++ UniversalFramework/Core/Testing/Tests/SuiteOutcome.cs
@@ -24,7 +24,7 @@ namespace Unicorn.Core.Testing.Tests
 
         public void FillWithTestsResults(List<Test> testsList)
         {
-            HashSet<string> bugsList = new HashSet<string>();
+            List<string> bugsList = new List<string>();
             foreach (Test test in testsList)
             {
                 if (test.Outcome.Result == Result.FAILED)
@@ -39,26 +39,6 @@ namespace Unicorn.Core.Testing.Tests
 
             Bugs = new string[bugsList.Count];
             bugsList.CopyTo(Bugs);
-
-
-            /*Logger.Instance.Info("============================================");
-            foreach (Test test in testsList)
-            {
-                Logger.Instance.Info("Test:" + test.Description);
-                Logger.Instance.Info("Result:" + test.Outcome.Result.ToString());
-                Logger.Instance.Info("Exec time:" + test.Outcome.ExecutionTime.ToString());
-
-                if(test.Outcome.Exception != null)
-                    Logger.Instance.Info("Exception:" + test.Outcome.Exception.ToString());
-
-                if (test.Outcome.Screenshot != null)
-                    Logger.Instance.Info("Screenshot:" + test.Outcome.Screenshot.ToString());
-
-                if (test.Outcome.Bugs != null)
-                    Logger.Instance.Info("Bugs:" + string.Join(",", test.Outcome.Bugs));
-                Logger.Instance.Info("\n\n\n");
-            }
-            //TODO:*/
         }
     }
 }
