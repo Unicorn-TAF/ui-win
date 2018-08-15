6343a9cf45ce05ab2e6c8de1d8db43282b75b09e
Vitaliy Dobriyan <dobriy88@yandex.ru>
2018-08-15 08:09:08 +0400

    added ability to init non public controls

--- src/Unicorn.UI/Core/PageObject/ContainerFactory.cs
+++ src/Unicorn.UI/Core/PageObject/ContainerFactory.cs
@@ -8,15 +8,18 @@ namespace Unicorn.UI.Core.PageObject
     {
         public static void InitContainer<T>(T container)
         {
-            FieldInfo[] fields = container.GetType().GetFields();
+            FieldInfo[] fields = container.GetType()
+                .GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
             foreach (FieldInfo field in fields)
             {
-                var attributes = field.GetCustomAttributes(typeof(FindAttribute), true) as FindAttribute[];
-                if (attributes.Length != 0)
+                var findAttribute = field.GetCustomAttribute(typeof(FindAttribute), true) as FindAttribute;
+
+                if (findAttribute != null)
                 {
                     Type controlType = field.FieldType;
                     var control = Activator.CreateInstance(controlType);
-                    ((IControl)control).Locator = attributes[0].Locator;
+                    ((IControl)control).Locator = findAttribute.Locator;
                     ((IControl)control).Cached = false;
 
                     var contextField = control.GetType().GetProperty("ParentSearchContext", BindingFlags.Public | BindingFlags.Instance);
