98be223dede7a5525aba5fa277648e82fa85aca5
Vitaliy Dobriyan <dobriy88@yandex.ru>
2018-10-12 18:32:45 +0400

    Added TextInput element Web implementation

---
+++ src/Unicorn.UI/Web/Controls/Typified/TextInput.cs
@@ -0,0 +1,21 @@
+﻿using Unicorn.UI.Core.Controls.Interfaces.Typified;
+
+namespace Unicorn.UI.Web.Controls.Typified
+{
+    public class TextInput : WebControl, ITextInput
+    {
+        public virtual string Value => this.Instance.GetAttribute("value");
+
+        public virtual void SendKeys(string text)
+        {
+            this.Instance.SendKeys(text);
+        }
+
+        public virtual bool SetText(string text)
+        {
+            this.Instance.Clear();
+            this.Instance.SendKeys(text);
+            return true;
+        }
+    }
+}
