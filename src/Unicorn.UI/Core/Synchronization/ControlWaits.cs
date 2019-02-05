c06f2344f4168ee5863602c2d245fec571bdd6b3
Vitaliy Dobriyan <dobriy88@yandex.ru>
2019-02-05 09:11:07 +0400

    Expanded controls waits

--- src/Unicorn.UI/Core/Synchronization/ControlWaits.cs
+++ src/Unicorn.UI/Core/Synchronization/ControlWaits.cs
@@ -5,7 +5,15 @@ namespace Unicorn.UI.Core.Synchronization
 {
     public static class ControlWaits
     {
-        public static TReturn Wait<TTarget, TReturn>(this TTarget control, Func<TTarget, TReturn> command, TimeSpan commandTimeout, TimeSpan pollingInterval, Type ignoreException, string message = null) where TTarget : IControl
+        #region Control state waits
+
+        public static TReturn Wait<TTarget, TReturn>(
+            this TTarget control, 
+            Func<TTarget, TReturn> command, 
+            TimeSpan commandTimeout, 
+            TimeSpan pollingInterval, 
+            Type ignoreException, 
+            string message) where TTarget : IControl
         {
             var wait = new UiWait<TTarget>(control)
             {
@@ -19,7 +27,20 @@ namespace Unicorn.UI.Core.Synchronization
             return wait.Until(command);
         }
 
-        public static TReturn Wait<TTarget, TReturn>(this TTarget control, Func<TTarget, TReturn> command, TimeSpan commandTimeout, TimeSpan pollingInterval, string message = null) where TTarget : IControl
+        public static TReturn Wait<TTarget, TReturn>(
+            this TTarget control, 
+            Func<TTarget, TReturn> command, 
+            TimeSpan commandTimeout, 
+            TimeSpan pollingInterval, 
+            Type ignoreException) where TTarget : IControl =>
+            Wait(control, command, commandTimeout, pollingInterval, ignoreException, string.Empty);
+
+        public static TReturn Wait<TTarget, TReturn>(
+            this TTarget control, 
+            Func<TTarget, TReturn> command, 
+            TimeSpan commandTimeout, 
+            TimeSpan pollingInterval, 
+            string message) where TTarget : IControl
         {
             var wait = new UiWait<TTarget>(control)
             {
@@ -31,7 +52,38 @@ namespace Unicorn.UI.Core.Synchronization
             return wait.Until(command);
         }
 
-        public static TReturn Wait<TTarget, TReturn>(this TTarget control, Func<TTarget, TReturn> command, string message = null) where TTarget : IControl
+        public static TReturn Wait<TTarget, TReturn>(
+            this TTarget control, 
+            Func<TTarget, TReturn> command, 
+            TimeSpan commandTimeout, 
+            TimeSpan pollingInterval) where TTarget : IControl =>
+            Wait(control, command, commandTimeout, pollingInterval, string.Empty);
+
+        public static TReturn Wait<TTarget, TReturn>(
+            this TTarget control,
+            Func<TTarget, TReturn> command,
+            TimeSpan commandTimeout,
+            string message) where TTarget : IControl
+        {
+            var wait = new UiWait<TTarget>(control)
+            {
+                Message = message,
+                Timeout = commandTimeout
+            };
+
+            return wait.Until(command);
+        }
+
+        public static TReturn Wait<TTarget, TReturn>(
+            this TTarget control,
+            Func<TTarget, TReturn> command,
+            TimeSpan commandTimeout) where TTarget : IControl =>
+            Wait(control, command, commandTimeout, string.Empty);
+
+        public static TReturn Wait<TTarget, TReturn>(
+            this TTarget control, 
+            Func<TTarget, TReturn> command, 
+            string message) where TTarget : IControl
         {
             var wait = new UiWait<TTarget>(control)
             {
@@ -41,7 +93,106 @@ namespace Unicorn.UI.Core.Synchronization
             return wait.Until(command);
         }
 
-        public static TReturn Wait<TTarget, TReturn>(this TTarget control, Func<TTarget, string, string, TReturn> command, string attribute, string value, string message = null) where TTarget : IControl
+        public static TReturn Wait<TTarget, TReturn>(
+            this TTarget control, 
+            Func<TTarget, TReturn> command) where TTarget : IControl =>
+            Wait(control, command, string.Empty);
+
+        #endregion
+
+        #region Control attribute waits
+
+        public static TReturn Wait<TTarget, TReturn>(
+            this TTarget control,
+            Func<TTarget, string, string, TReturn> command,
+            string attribute,
+            string value,
+            TimeSpan commandTimeout,
+            TimeSpan pollingInterval,
+            Type ignoreException,
+            string message) where TTarget : IControl
+        {
+            var wait = new UiWait<TTarget>(control, attribute, value)
+            {
+                Message = message,
+                PollingInterval = pollingInterval,
+                Timeout = commandTimeout,
+            };
+
+            wait.IgnoreExceptionTypes(ignoreException);
+
+            return wait.UntilAttribute(command);
+        }
+
+        public static TReturn Wait<TTarget, TReturn>(
+            this TTarget control,
+            Func<TTarget, string, string, TReturn> command,
+            string attribute,
+            string value,
+            TimeSpan commandTimeout,
+            TimeSpan pollingInterval,
+            Type ignoreException) where TTarget : IControl =>
+            Wait(control, command, attribute, value, commandTimeout, pollingInterval, ignoreException, string.Empty);
+
+        public static TReturn Wait<TTarget, TReturn>(
+            this TTarget control,
+            Func<TTarget, string, string, TReturn> command,
+            string attribute,
+            string value,
+            TimeSpan commandTimeout,
+            TimeSpan pollingInterval,
+            string message) where TTarget : IControl
+        {
+            var wait = new UiWait<TTarget>(control, attribute, value)
+            {
+                Message = message,
+                PollingInterval = pollingInterval,
+                Timeout = commandTimeout
+            };
+
+            return wait.UntilAttribute(command);
+        }
+
+        public static TReturn Wait<TTarget, TReturn>(
+            this TTarget control,
+            Func<TTarget, string, string, TReturn> command,
+            string attribute,
+            string value,
+            TimeSpan commandTimeout,
+            TimeSpan pollingInterval) where TTarget : IControl =>
+            Wait(control, command, attribute, value, commandTimeout, pollingInterval, string.Empty);
+
+        public static TReturn Wait<TTarget, TReturn>(
+            this TTarget control,
+            Func<TTarget, string, string, TReturn> command,
+            string attribute,
+            string value,
+            TimeSpan commandTimeout,
+            string message) where TTarget : IControl
+        {
+            var wait = new UiWait<TTarget>(control, attribute, value)
+            {
+                Message = message,
+                Timeout = commandTimeout
+            };
+
+            return wait.UntilAttribute(command);
+        }
+
+        public static TReturn Wait<TTarget, TReturn>(
+            this TTarget control,
+            Func<TTarget, string, string, TReturn> command,
+            string attribute,
+            string value,
+            TimeSpan commandTimeout) where TTarget : IControl =>
+            Wait(control, command, attribute, value, commandTimeout, string.Empty);
+
+        public static TReturn Wait<TTarget, TReturn>(
+            this TTarget control, 
+            Func<TTarget, string, string, TReturn> command, 
+            string attribute, 
+            string value, 
+            string message) where TTarget : IControl
         {
             var wait = new UiWait<TTarget>(control, attribute, value)
             {
@@ -50,5 +201,14 @@ namespace Unicorn.UI.Core.Synchronization
 
             return wait.UntilAttribute(command);
         }
+
+        public static TReturn Wait<TTarget, TReturn>(
+            this TTarget control,
+            Func<TTarget, string, string, TReturn> command,
+            string attribute,
+            string value) where TTarget : IControl =>
+            Wait(control, command, attribute, value, string.Empty);
+
+        #endregion
     }
 }
