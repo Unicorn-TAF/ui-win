9a8107b9f3a0d66db793323dfb72274b1f984485
Vitaliy <dobriy88@yandex.ru>
2020-03-03 18:58:41 +0400

    added demo project as example

---
+++ example/Demo.Celestia/Ui/Common/FooterBlock.cs
@@ -0,0 +1,25 @@
+﻿using Unicorn.UI.Core.Driver;
+using Unicorn.UI.Core.PageObject;
+using Unicorn.UI.Web.Controls;
+
+namespace Demo.Celestia.Ui.Common
+{
+    public class FooterBlock : WebContainer
+    {
+        [Name("Twitter link")]
+        [Find(Using.WebCss, "a.fa-twitter")]
+        public WebControl LinkTwitter { get; set; }
+
+        [Name("GitHub link")]
+        [Find(Using.WebCss, "a.fa-github")]
+        public WebControl LinkGithub { get; set; }
+
+        [Name("Email link")]
+        [Find(Using.WebCss, "a.fa-envelope")]
+        public WebControl LinkEmail { get; set; }
+
+        [Name("Copyright")]
+        [Find(Using.WebCss, "ul.copyright>li")]
+        public WebControl Copyright { get; set; }
+    }
+}
---
+++ example/Demo.Dialer/Ui/DialPad.cs
@@ -0,0 +1,50 @@
+﻿using Unicorn.UI.Core.Driver;
+using Unicorn.UI.Core.PageObject;
+using Unicorn.UI.Mobile.Android.Controls;
+
+namespace Demo.AndroidDialer.Ui
+{
+    public class DialPad : AndroidContainer
+    {
+        [Find(Using.Id, "com.android.dialer:id/one")]
+        public AndroidControl ButtonOne { get; set; }
+
+        [Find(Using.Id, "com.android.dialer:id/two")]
+        public AndroidControl ButtonTwo { get; set; }
+
+        [Find(Using.Id, "com.android.dialer:id/three")]
+        public AndroidControl ButtonThree { get; set; }
+
+        [Find(Using.Id, "com.android.dialer:id/four")]
+        public AndroidControl ButtonFour { get; set; }
+
+        [Find(Using.Id, "com.android.dialer:id/five")]
+        public AndroidControl ButtonFive { get; set; }
+
+        [Find(Using.Id, "com.android.dialer:id/six")]
+        public AndroidControl ButtonSix { get; set; }
+
+        [Find(Using.Id, "com.android.dialer:id/seven")]
+        public AndroidControl ButtonSeven { get; set; }
+
+        [Find(Using.Id, "com.android.dialer:id/eight")]
+        public AndroidControl ButtonEight { get; set; }
+
+        [Find(Using.Id, "com.android.dialer:id/nine")]
+        public AndroidControl ButtonNine { get; set; }
+
+        [Find(Using.Id, "com.android.dialer:id/star")]
+        public AndroidControl ButtonStar { get; set; }
+
+        [Find(Using.Id, "com.android.dialer:id/pound")]
+        public AndroidControl ButtonPound { get; set; }
+
+        [Find(Using.Id, "com.android.dialer:id/digits")]
+        public AndroidControl InputNumber { get; set; }
+
+        public AndroidControl GetButton(string name)
+        {
+            return this.Find<AndroidControl>(ByLocator.Id($"com.android.dialer:id/{name}"));
+        }
+    }
+}
