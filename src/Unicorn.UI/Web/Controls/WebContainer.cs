920e2da408479e51dbe49821ad812dd349cd47f3
Vitaliy Dobriyan <dobriy88@yandex.ru>
2021-02-19 23:35:29 +0300

    E-38: WebContainer basic methods implementation

--- src/Unicorn.UI/Web/Controls/WebContainer.cs
+++ src/Unicorn.UI/Web/Controls/WebContainer.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
+using Unicorn.UI.Core.Controls;
 using Unicorn.UI.Core.Driver;
 using Unicorn.UI.Core.PageObject;
+using Unicorn.UI.Web.Controls.Typified;
 
 namespace Unicorn.UI.Web.Controls
 {
@@ -27,42 +28,47 @@ namespace Unicorn.UI.Web.Controls
         }
 
         /// <summary>
-        /// Clicks button with specified text within the container.
+        /// Clicks button with specified text.<para/>
+        /// Default locator is: .//button[normalize-space() = 'BUTTON_TEXT']
         /// </summary>
-        /// <param name="locator">button text</param>
-        public virtual void ClickButton(string locator) =>
-            Find<WebControl>(ByLocator.Xpath($".//button[. = '{locator}']"))
+        /// <param name="name">button text</param>
+        /// <exception cref="ControlNotFoundException">is thrown when control was not found</exception>
+        public virtual void ClickButton(string name) =>
+            Find<WebControl>(ByLocator.Xpath($".//button[normalize-space() = '{name}']"))
             .Click();
 
         /// <summary>
-        /// Sets specified text into specified text input within the container.
+        /// Sets specified text into text input with specified placeholder.<para/>
+        /// Default locator is: .//input[@placeholder='INPUT_PLACEHOLDER']
         /// </summary>
-        /// <param name="locator">text input name</param>
+        /// <param name="name">text input placeholder text</param>
         /// <param name="text">text to input</param>
-        public virtual void InputText(string locator, string text)
-        {
-            throw new NotImplementedException();
-        }
+        /// <exception cref="ControlNotFoundException">is thrown when control was not found</exception>
+        public virtual void InputText(string name, string text) =>
+            Find<TextInput>(ByLocator.Xpath($".//input[@placeholder='{name}']"))
+            .SetValue(text);
 
         /// <summary>
-        /// Selects specified radio button within the container.
+        /// Selects specified radio button by label text.<para/>
+        /// Default locator is: .//input[@type='radio' and ..//*[. = 'RADIO_LABEL_TEXT']]
         /// </summary>
-        /// <param name="locator">radio button name</param>
+        /// <param name="name">radio button label</param>
+        /// <exception cref="ControlNotFoundException">is thrown when control was not found</exception>
         /// <returns>true - if selection was made; false - if already selected</returns>
-        public virtual bool SelectRadio(string locator)
-        {
-            throw new NotImplementedException();
-        }
+        public virtual bool SelectRadio(string name) =>
+            Find<Radio>(ByLocator.Xpath($".//input[@type='radio' and ..//*[. = '{name}']]"))
+            .Select();
 
         /// <summary>
-        /// Sets specified checkbox within the container in specified state.
+        /// Sets specified state for checkbox with specified name.<para/>
+        /// Default locator is: .//input[@type='checkbox' and @name='CHECKBOX_NAME']
         /// </summary>
-        /// <param name="locator">checkbox name</param>
+        /// <param name="name">checkbox name</param>
         /// <param name="state">state to set for checkbox</param>
+        /// <exception cref="ControlNotFoundException">is thrown when control was not found</exception>
         /// <returns>true - if state was changed; false - if already in desired state</returns>
-        public virtual bool SetCheckbox(string locator, bool state)
-        {
-            throw new NotImplementedException();
-        }
+        public virtual bool SetCheckbox(string name, bool state) =>
+            Find<Checkbox>(ByLocator.Xpath($".//input[@type='checkbox' and @name='{name}']"))
+            .SetCheckedState(state);
     }
 }
