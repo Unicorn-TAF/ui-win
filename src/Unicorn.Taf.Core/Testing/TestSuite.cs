f05949d93a81b6bdeef31a1cba354e9e98285765
Vitaliy Dobriyan <dobriy88@yandex.ru>
2021-10-21 11:17:31 +0300

    Fixes: ordering unit test, suite timeouts handling logic

--- src/Unicorn.Taf.Core/Testing/TestSuite.cs
+++ src/Unicorn.Taf.Core/Testing/TestSuite.cs
@@ -251,16 +251,17 @@ namespace Unicorn.Taf.Core.Testing
             {
                 test.Skip();
                 Logger.Instance.Log(LogLevel.Warning, $"Test '{test.Outcome.Title}' {test.Outcome.Result}");
-                return;
             }
-
-            test.Execute(this);
-
-            RunAftertests(test.Outcome);
-
-            if (test.Outcome.Result == Status.Failed)
+            else
             {
-                Outcome.Result = Status.Failed;
+                test.Execute(this);
+
+                RunAftertests(test.Outcome);
+
+                if (test.Outcome.Result == Status.Failed)
+                {
+                    Outcome.Result = Status.Failed;
+                }
             }
 
             Outcome.TestsOutcomes.Add(test.Outcome);
