f2ddfd379444cbbeb31c73b6052596d9060b30d7
Vitaliy Dobriyan <dobriy88@yandex.ru>
2021-11-29 10:49:44 +0300

    Example: added rest service tests base

---
+++ example/Demo.DummyRestApi/DummyApiClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using Unicorn.Backend.Services.RestService;
+
+namespace Demo.DummyRestApi
+{
+    public class DummyApiClient : RestClient
+    {
+        public DummyApiClient() : base("http://dummy.restapiexample.com")
+        {
+
+        }
+
+        public RestResponse GetEmployee(int id) =>
+            SendRequest(HttpMethod.Get, $"/api/v1/employee/{id}");
+    }
+}
---
+++ example/Demo.Tests/RestService/DummyRestApiTests.cs
@@ -0,0 +1,31 @@
+﻿using Demo.Tests.Base;
+using System.Net.Http;
+using Unicorn.Backend.Matchers;
+using Unicorn.Backend.Services.RestService;
+using Unicorn.Taf.Core.Testing;
+using Unicorn.Taf.Core.Testing.Attributes;
+
+namespace Demo.Tests.RestService
+{
+    /// <summary>
+    /// Rest web service test suite example.
+    /// The test suite should inherit <see cref="TestSuite"/> and have <see cref="SuiteAttribute"/>
+    /// It's possible to specify any number of suite tags and metadata.
+    /// </summary>
+    [Suite("Dummy Rest Api tests")]
+    [Tag("Rest"), Tag("DummyApi")]
+    [Metadata(key: "Description", value: "Tests for dummy rest api functionality")]
+    [Metadata(key: "Api link", value: "http://dummy.restapiexample.com")]
+    public class DummyRestApiTests : BaseTestSuite
+    {
+        [Test]
+        [Author("Vitaliy Dobriyan")]
+        public void GetEmployeeByIdTest()
+        {
+            RestResponse employee = Do.DummyRestApi.GetEmployee(1);
+            Do.Assertion.AssertThat(
+                employee,
+                Service.Rest.Response.HasTokenWithValue("$.status", "success"));
+        }
+    }
+}
