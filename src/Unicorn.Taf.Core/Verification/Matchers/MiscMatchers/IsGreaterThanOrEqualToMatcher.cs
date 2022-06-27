de8dfffeccdb4486f5c63aff1986f797d5d9d0fc
Vitaliy Dobriyan <dobriy88@yandex.ru>
2022-06-27 17:28:39 +0300

    Core: added Greater/Less than (or equal to) matchers

---
+++ src/Unicorn.Taf.Core/Verification/Matchers/MiscMatchers/IsGreaterThanOrEqualToMatcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Unicorn.Taf.Core.Verification.Matchers.MiscMatchers
+{
+    /// <summary>
+    /// Matcher to check if <see cref="IComparable"/> is greater than or equal to other <see cref="IComparable"/>.
+    /// </summary>
+    public class IsGreaterThanOrEqualToMatcher : TypeSafeMatcher<IComparable>
+    {
+        private readonly IComparable _compareTo;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IsGreaterThanOrEqualToMatcher"/> class with specified object to compare.
+        /// </summary>
+        /// <param name="compareTo">object to compare</param>
+        public IsGreaterThanOrEqualToMatcher(IComparable compareTo)
+        {
+            _compareTo = compareTo;
+        }
+
+        /// <summary>
+        /// Gets check description.
+        /// </summary>
+        public override string CheckDescription => $"Is greater than or equal to {_compareTo}";
+
+        /// <summary>
+        /// Checks if <see cref="IComparable"/> is greater than or equal to other.
+        /// </summary>
+        /// <param name="actual">object under assertion</param>
+        /// <returns>true - if <see cref="IComparable"/> is greater than or equal to other; otherwise - false</returns>
+        public override bool Matches(IComparable actual)
+        {
+            DescribeMismatch(actual.ToString());
+            return actual.CompareTo(_compareTo) >= 0;
+        }
+    }
+}
---
+++ src/Unicorn.Taf.Core/Verification/Matchers/MiscMatchers/IsLessThanOrEqualToMatcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Unicorn.Taf.Core.Verification.Matchers.MiscMatchers
+{
+    /// <summary>
+    /// Matcher to check if <see cref="IComparable"/> is less than or equal to other <see cref="IComparable"/>.
+    /// </summary>
+    public class IsLessThanOrEqualToMatcher : TypeSafeMatcher<IComparable>
+    {
+        private readonly IComparable _compareTo;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IsLessThanOrEqualToMatcher"/> class with specified object to compare.
+        /// </summary>
+        /// <param name="compareTo">object to compare</param>
+        public IsLessThanOrEqualToMatcher(IComparable compareTo)
+        {
+            _compareTo = compareTo;
+        }
+
+        /// <summary>
+        /// Gets check description.
+        /// </summary>
+        public override string CheckDescription => $"Is less than or equal to {_compareTo}";
+
+        /// <summary>
+        /// Checks if <see cref="IComparable"/> is less than or equal to other.
+        /// </summary>
+        /// <param name="actual">object under assertion</param>
+        /// <returns>true - if <see cref="IComparable"/> is less than or equal to other; otherwise - false</returns>
+        public override bool Matches(IComparable actual)
+        {
+            DescribeMismatch(actual.ToString());
+            return actual.CompareTo(_compareTo) <= 0;
+        }
+    }
+}
