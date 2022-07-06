99742511ef1ccc99f988f08e6a193cbc3f95ff26
Vitaliy Dobriyan <dobriy88@yandex.ru>
2022-07-06 12:45:28 +0300

    UI.Core: added documentation for ControlExtension

--- src/Unicorn.UI/Core/PageObject/ControlExtension.cs
+++ src/Unicorn.UI/Core/PageObject/ControlExtension.cs
@@ -3,8 +3,18 @@ using Unicorn.UI.Core.Driver;
 
 namespace Unicorn.UI.Core.PageObject
 {
+    /// <summary>
+    /// Extensions for <see cref="IControl"/>
+    /// </summary>
     public static class ControlExtension
     {
+        /// <summary>
+        /// Check if control exists int page object immediately ignoring implicitly wait.<br/>
+        /// Works only for controls located by <see cref="FindAttribute"/>.
+        /// </summary>
+        /// <typeparam name="T">control type (should implement <see cref="IControl"/>)</typeparam>
+        /// <param name="control">Control instance</param>
+        /// <returns>true - if control exists; otherwise - false</returns>
         public static bool ExistsInPageObject<T>(this T control) where T : IControl
         {
             ISearchContext context = control.GetType()
