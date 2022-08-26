0e32b724fdec8c684b077c65ec13d98863bfd640
Vitaliy Dobriyan <dobriy88@yandex.ru>
2022-08-26 22:49:52 +0300

    pack bat file moved to tools and taf root is automatically generated

--- tools/pack.bat
+++ tools/pack.bat
@@ -1,6 +1,6 @@
 @echo off
-set TAF_ROOT=%1
-set OUT_DIR=%2
+set TAF_ROOT=%cd%\..\src
+set OUT_DIR=%1
 
 for %%p in (Unicorn.Taf.Api Unicorn.Taf.Core Unicorn.Backend) do (dotnet pack %TAF_ROOT%\%%p\%%p.csproj -o %OUT_DIR% -c Release --no-build -p:NuspecFile=%TAF_ROOT%\%%p\%%p.nuspec)
 for %%p in (Core Win Web Mobile) do (dotnet pack %TAF_ROOT%\Unicorn.UI\%%p\Unicorn.UI.%%p.csproj -o %OUT_DIR% -c Release --no-build -p:NuspecFile=%TAF_ROOT%\Unicorn.UI\%%p\Unicorn.UI.%%p.nuspec)
