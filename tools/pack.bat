df8df9234933e8e8272ba0579e3c95f5974e3827
Vitaliy Dobriyan <dobriy88@yandex.ru>
2024-05-25 00:43:02 +0300

    improved packing batch, updated UI.Core dependency, removed useless deps from unit tests

--- tools/pack.bat
+++ tools/pack.bat
@@ -1,6 +1,7 @@
 @echo off
 set PKG=Unicorn.UI.Win
 set PROJ_PATH=%cd%\..\src\%PKG%\%PKG%
-set OUT_DIR=%1
+set VERSION=%1
+set OUT_DIR=%2
 
-dotnet pack %PROJ_PATH%.csproj -o %OUT_DIR% -c Release -p:NuspecFile=%PROJ_PATH%.nuspec
+dotnet pack %PROJ_PATH%.csproj -o %OUT_DIR% -c Release -p:NuspecFile=%PROJ_PATH%.nuspec -p:Version=%VERSION%
